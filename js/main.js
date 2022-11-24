0972d6d67f5598fd56f7019f41bda9538b1a5711
Wolcken_Git <Alfredoxd00@hotmail.com>
2022-11-24 16:04:48 -0400

    Inicio_examen_js

---
+++ js/main.js
@@ -0,0 +1,13 @@
+/*
+ Author: Diego Adrian<adry.dabp@gmail.com>
+ Final Test: Bases de javascript
+ Date: 23/11/2022
+ University: USIP
+*/
+
+console.log("Aqui ira todo tu codigo...EXITOS !!!");
+
+const btn = document.getElementById('calcular');
+btn.addEventListener('click', () => {
+  alert('Mensaje de prueba...');
+})
