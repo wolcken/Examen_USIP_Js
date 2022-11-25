de28135c806c03a80a7ce44204359582613d2c98
Wolcken_Git <Alfredoxd00@hotmail.com>
2022-11-24 22:19:54 -0400

    Resuelto con Funciones de Llamada

--- js/main.js
+++ js/main.js
@@ -18,119 +18,36 @@ const val_mex = [0.36,46.97,1,8.52,0.052];
 const val_arg = [0.042,5.51,0.12,1,0.0061];
 const val_dol = [6.91,910.35,19.38,165.20,1];
 
+const moneda_cambio = [val_bolv, val_chil, val_mex, val_arg, val_dol];
+
+function moneda(val_introducido, de_i, a_i){
+  //alert(de_i +" "+ a_i + "       "+moneda_cambio[de_i]+"  "+moneda_cambio[de_i][a_i]);
+  switch(a_i){
+    case '0':
+      return  val_introducido * moneda_cambio[de_i][a_i];
+    
+    case '1':
+      return val_introducido * moneda_cambio[de_i][a_i];
+    
+    case '2':
+      return val_introducido * moneda_cambio[de_i][a_i];
+    
+    case '3':
+      return val_introducido * moneda_cambio[de_i][a_i];
+    
+    case '4':
+      return val_introducido * moneda_cambio[de_i][a_i];
+  }
+}
+
 function calcular(){
+
   var val_introducido = parseInt(document.getElementById('monto').value);
 
   var de_i = document.getElementById('de').value;
   var a_i = document.getElementById('a').value;
 
-  var resultado = 0;
-
-  if(de_i == '1'){
-    switch(a_i){
-      case '1':
-        resultado = val_introducido * val_bolv[0];
-      break;
-      case '2':
-        resultado = val_introducido * val_bolv[1];
-      break;
-      case '3':
-        resultado = val_introducido * val_bolv[2];
-      break;
-      case '4':
-        resultado = val_introducido * val_bolv[3];
-      break;
-      case '5':
-        resultado = val_introducido * val_bolv[4];
-      break;
-    }
-  }
-  else{
-    if(de_i == '2'){
-      switch(a_i){
-        case '1':
-          resultado = val_introducido * val_chil[0];
-        break;
-        case '2':
-          resultado = val_introducido * val_chil[1];
-        break;
-        case '3':
-          resultado = val_introducido * val_chil[2];
-        break;
-        case '4':
-          resultado = val_introducido * val_chil[3];
-        break;
-        case '5':
-          resultado = val_introducido * val_chil[4];
-        break;
-      }
-    }
-    else{
-      if(de_i == '3'){
-        switch(a_i){
-          case '1':
-            resultado = val_introducido * val_mex[0];
-          break;
-          case '2':
-            resultado = val_introducido * val_mex[1];
-          break;
-          case '3':
-            resultado = val_introducido * val_mex[2];
-          break;
-          case '4':
-            resultado = val_introducido * val_mex[3];
-          break;
-          case '5':
-            resultado = val_introducido * val_mex[4];
-          break;
-        }
-      }
-      else{
-        if(de_i == '4'){
-          switch(a_i){
-            case '1':
-              resultado = val_introducido * val_arg[0];
-            break;
-            case '2':
-              resultado = val_introducido * val_arg[1];
-            break;
-            case '3':
-              resultado = val_introducido * val_arg[2];
-            break;
-            case '4':
-              resultado = val_introducido * val_arg[3];
-            break;
-            case '5':
-              resultado = val_introducido * val_arg[4];
-            break;
-          }
-        }
-        else{
-          if(de_i == '5'){
-            switch(a_i){
-              case '1':
-                resultado = val_introducido * val_dol[0];
-              break;
-              case '2':
-                resultado = val_introducido * val_dol[1];
-              break;
-              case '3':
-                resultado = val_introducido * val_dol[2];
-              break;
-              case '4':
-                resultado = val_introducido * val_dol[3];
-              break;
-              case '5':
-                resultado = val_introducido * val_dol[4];
-              break;
-            }
-          }
-        }
-      }
-    }
-  }
-
+  var resultado = moneda(val_introducido, de_i, a_i);
   
-
   return resultado;
 }
